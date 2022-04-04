2db5bca379b4e281aac258c5b397476651e93ad0
Giuseppe <giuseppefvr@gmail.com>
2022-04-04 23:54:28 +0200

    feat: fetch API with change event

--- js/script.js
+++ js/script.js
@@ -12,3 +12,26 @@ const contentComuneDiv = document.querySelector(".content__comune");
 if (comuniSelect.value === "select") {
     contentComuneDiv.innerHTML = "Please select a comune";
 }
+
+
+// Fetch API with change event
+comuniSelect.addEventListener("change", (e) => {
+
+    const comuneSelected = e.target.value;
+
+    if (comuneSelected === "select") {
+        contentComuneDiv.innerHTML = "Please select a comune";
+    } else {
+
+        // console.log(comuneSelected)
+
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${comuneSelected}&appid=1cbb1add7327a490fe3c4104696a56aa`)
+        .then(response => response.json())
+        .then(data => {
+            contentComuneDiv.innerHTML = data.weather[0].description + data.name;
+            console.log(data);
+        })
+
+    }
+
+})
