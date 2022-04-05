b17fe2ae03e49b3f85206bc758681ab490f0498c
Giuseppe Favarò <101141969+giuseppefavaro@users.noreply.github.com>
2022-04-05 19:13:05 +0200

    feat: added style and optimization single page (#1)

--- js/render.js
+++ js/render.js
@@ -14,8 +14,45 @@ const createSelect = (comuni) => {
 
 
 const createSinglePage = (data) => {
-    const boxDiv = document.querySelector(".box");
-    boxDiv.innerHTML = data.weather[0].description + data.main.temp +  data.main.temp_max +  data.main.temp_min +  data.main.humidity +  data.visibility  +  data.wind.speed  + data.main.sea_level;
+    const content = document.querySelector(".content");
+
+    // console.log(data)
+
+    content.innerHTML = `
+
+    <section class="box">
+    <h1 class="box__h1">${data.name}</h1>
+
+    <div class="box__content">
+        <div class="box__img">
+            <span class="temperature">${data.main.temp}</span>
+            <figure>
+                <img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png" width="200" height="200" alt="weather">
+                <figcaption>${data.weather[0].main}</figcaption>
+            </figure>
+        </div>
+
+        <ul class="box__info">
+            <li><strong>Description:</strong> ${data.weather[0].description}</li>
+            <li><strong>Wind:</strong>  ${data.wind.speed}</li>
+            <li><strong>Max:</strong> ${data.main.temp_max}</li>
+            <li><strong>Min:</strong>  ${data.main.temp_min}</li>
+        </ul>
+    </div>
+</section>
+
+
+<section class="box">
+    <h2 class="box__h2">Other details</h2>
+
+    <ul class="box__info box__info--full">
+        <li><strong>Humidity:</strong> ${data.main.humidity}</li>
+        <li><strong>Visibility:</strong> ${data.main.visibility}</li>     
+        <li><strong>Sea Level:</strong> ${data.main.sea_level}</li>
+    </ul>
+</section>
+`;
+
 }
 
 
