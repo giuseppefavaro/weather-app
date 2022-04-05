add7abffd3e42ae8e0afe1ef1798dcf5eecd19e3
Giuseppe <giuseppefvr@gmail.com>
2022-04-05 18:11:24 +0200

    fix: footer at the bottom on mobile

--- js/script.js
+++ js/script.js
@@ -7,10 +7,10 @@ createSelect(comuniAG);
 
 // Show message when no comune is selected
 const comuniSelect = document.querySelector("#comuni-select");
-const contentComuneDiv = document.querySelector(".content__comune");
+const boxDiv = document.querySelector(".box");
 
 if (comuniSelect.value === "select") {
-    contentComuneDiv.innerHTML = "Please select a comune";
+    boxDiv.innerHTML = "Please select a comune";
 }
 
 
@@ -20,7 +20,7 @@ comuniSelect.addEventListener("change", (e) => {
     const comuneSelected = e.target.value;
 
     if (comuneSelected === "select") {
-        contentComuneDiv.innerHTML = "Please select a comune";
+        boxDiv.innerHTML = "Please select a comune";
     } else {
 
         // console.log(comuneSelected)
@@ -28,7 +28,6 @@ comuniSelect.addEventListener("change", (e) => {
         fetch(`https://api.openweathermap.org/data/2.5/weather?q=${comuneSelected}&appid=1cbb1add7327a490fe3c4104696a56aa`)
         .then(response => response.json())
         .then(data => {
-            contentComuneDiv.innerHTML = data.weather[0].description + data.name;
             console.log(data);
         })
 
